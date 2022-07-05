e9e9d92e6a5117e1c8d5fe4f727615ba4090248b
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-07-05 19:20:22 +0300

    2

--- src/components/praktick/Practicks,styled.jsx
+++ src/components/praktick/Practicks,styled.jsx
@@ -1,24 +1,35 @@
 import styled from 'styled-components';
 export const Button = styled.button`
-  font-size: 20px;
+  font-size: 25px;
   text-align: center;
   color: palevioletred;
   padding: 15px;
   border-radius: 10px
   margin-right:30px;
  cursor: pointer;
+ border:none;
+ background-color: #cdc9c9;
   transition-property: transform, box-shadow;
   transition-duration: 0.25s;
   transition-timing-function: 0.25s cubic-bezier(0.7, 0.98, 0.86, 0.98);
  &:hover {
-    transform: scale(1.02);
     box-shadow: 0 9px 47px 11px rgba(51, 51, 51, 0.18);}
 `;
-export const Meaning = styled.span`
+export const Title = styled.h2`
     font-size: 50px;
     color: tomato;
     font-weight:800; 
-     margin-right:30px;
+`
+export const StatisticsSection = styled.div`
+    text-align:start;
+`
+export const StatisticsTitle = styled.h3`
+    color: blue;
+    font-size:30px;
+`
+export const StatisticItem = styled.p`
+    color: goldenrod;
+    font-size:25px;
 `
 export const Container = styled.div`
     text-align:center;
--- src/components/praktick/Praktick.jsx
+++ src/components/praktick/Praktick.jsx
@@ -1,33 +1,60 @@
 import React from "react";
-import {Container,Meaning, Button} from './Practicks,styled'
+import {Container,Title, Button,StatisticsSection, StatisticsTitle,StatisticItem} from './Practicks,styled'
 // const { Component } = require("react");
 
 
 class Counter extends React.Component{
+  state = {
+  good: 0,
+  neutral: 0,
+  bad: 0
+}
     static defaultProps = {
-        initialValue:0,
+        TotalValue: 0,
+        PositiveFeedBack:0,
     }
-    // static propTypes = {};
-        state = {value:this.props.initialValue,}
+    // // static propTypes = {};
+    //     state = {value:this.props.initialValue,}
  
-    hendleIncrement=() =>{
+    hendleGood=() =>{
         this.setState(prevState=>({
-           value: prevState.value+ 1,
+           good: prevState.good+ 1,
         }))
     }
-      hendleDicrement=() =>{
+     hendleNeutral=() =>{
         this.setState(prevState=>({
-           value: prevState.value- 1,
-       }))
+           neutral: prevState.neutral+ 1,
+        }))
     }
-    render () {
+     hendleBad=() =>{
+        this.setState(prevState=>({
+           bad: prevState.bad+ 1,
+        }))
+    }
+ 
+    render() {
+        const Total = this.state.good + this.state.neutral + this.state.bad;
+        const Persent = Math.round (((this.state.good + this.state.neutral) *100)/Total);
+        
         return (
             <Container>
-                <Meaning>{this.state.value}</Meaning>
+                <Title>Please leave feedback</Title>
                 <div>
-                    <Button type="button" onClick={this.hendleIncrement}>Збільшити на 1</Button>
-                     <Button type="button"onClick={this.hendleDicrement}>Зменшити на 1</Button>
+                    <Button type="button" onClick={ this.hendleGood}>Good</Button>
+                    <Button type="button" onClick={ this.hendleNeutral}>Neutral</Button>
+                    <Button type="button" onClick={ this.hendleBad}>Bad</Button>
                 </div>
+                <StatisticsSection>
+                    <StatisticsTitle>Statistics</StatisticsTitle>
+                    <StatisticItem>Good: {this.state.good}</StatisticItem>
+                    <StatisticItem>Neutral: {this.state.neutral}</StatisticItem>
+                    <StatisticItem>Bad: {this.state.bad}</StatisticItem>    
+                    <StatisticItem>Total: {Total}</StatisticItem>  
+                    <StatisticItem>Positive feedback: {Persent?Persent:0}%</StatisticItem>  
+                </StatisticsSection>
+                
+           
+               
             </Container>
         )
     }
